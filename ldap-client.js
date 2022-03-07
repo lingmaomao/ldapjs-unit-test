7a8bfe94d8cd44621884bcd06ca09fd314c4fba7
maomao.ling@intel.com <maomao.ling@intel.com>
2022-03-07 04:57:17 -0500

    ccr\maomaoli search is ok!
    Signed-off-by: maomao.ling@intel.com <maomao.ling@intel.com>

--- ldap-client.js
+++ ldap-client.js
@@ -6,30 +6,6 @@ ldap_admin_user = 'ccr\\sys_drsimon'
 ldap_admin_password = 'cce@fhm@drsimon_2022_2'
 
 function login(username, password) {
-	/*
-  user_split = username.split("\\")
-  console.log("user_split:", user_split)
-
-  ldap_base_search = 'OU=Workers,DC='+user_split[0]+',DC=corp,DC=intel,DC=com'
-  
-    var admin_client = ldap.createClient({
-      url: 'ldap://ccr.corp.intel.com'
-      port: ldap_port
-    })
-    const dn = user_split[1]+'@ccr.corp.intel.com'
-  	console.log("dn:", dn)
-    var SearchSuccess = false
-    //bind search account
-    client.bind(dn, password, function (err, res1) {
-      if (err) {
-      console.log('Error is:', err)
-        console.log("Incorrect username or password! (idsid, ccr\\maomaoli)");
-    }
-    })
-	*/
-
-
-
   if (!username || !password) {
   	console.log("Both idsid and Password are required!")
   }
@@ -38,9 +14,9 @@ function login(username, password) {
   user_split = username.split("\\")
   console.log("user_split:", user_split)
 
-  ldap_host = 'ldap://'+ user_split[0]+'.corp.intel.com'
+  ldap_host = 'ldap://ccr.corp.intel.com'
   console.log(ldap_host)
-  ldap_base_search = 'OU=Workers,DC='+user_split[0]+',DC=corp,DC=intel,DC=com'
+  ldap_base_search = 'OU=Workers,DC=ccr,DC=corp,DC=intel,DC=com'
   console.log("ldap_base_search:", ldap_base_search)
   
     //create client
@@ -53,9 +29,10 @@ function login(username, password) {
     var SearchSuccess = false
     //bind search account
     client.bind(dn, password, function (err, res1) {
+    //client.bind(user_split[1], ldap_admin_password, function (err, res1) {
       if (err) {
       console.log('Error is:', err)
-        console.log("Incorrect username or password! (idsid, ccr\\maomaoli)");
+        console.log("connect error, bind error");
 		  /*
       return reject({
         error: "WRONG_CREDENTIAL",
@@ -66,30 +43,34 @@ function login(username, password) {
     })
 
 	user='maomaoli'
+	//user='aaahmed'
+	
+	/*
     var opts = {
       filter:'(&(memberOf=CN=FMTool-AD,OU=Managed,OU=Groups,DC=amr,DC=corp,DC=intel,DC=com)(sAMAccountName='+user+'))',
       scope: 'sub',     
       timeLimit: 500   
     }
+	*/
+    var opts = {
+      filter:'(sAMAccountName='+user+')',
+      scope: 'sub',     
+      timeLimit: 500   
+    }
 
-    client.search('ou=Workers,dc=ccr,dc=corp,dc=intel,dc=com', opts, function (err, res2) {
+    //client.search('ou=Workers,dc=amr,dc=corp,dc=intel,dc=com', opts, function (err, res2) {
+    client.search(ldap_base_search, opts, function (err, res2) {
       res2.on('searchEntry', function (entry) {
         SearchSuccess = true
         var user = entry.object
-		  console.log("user:", user)
+		console.log("user.dn:", user.dn)
       })
 
       //search error
       res2.on('error', function (err) {
         SearchSuccess = false
         client.unbind()
-		  /*
-        return reject({
-          error: "WRONG_CREDENTIAL",
-          message: `You are not a member of FMTool-access, please register in AGS!`,
-        })
-		*/
-          console.log("You are not a member of FMTool-access, please register in AGS!")
+        console.log("You are not a member of FMTool-access, please register in AGS!")
       })
 
       //search end
