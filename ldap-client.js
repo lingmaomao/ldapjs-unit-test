e0bfc509be3833c2eb6310713999511d4b234074
maomao.ling@intel.com <maomao.ling@intel.com>
2022-03-08 00:21:53 -0500

    test amr, 10.239.87.4 network maybe not ok
    Signed-off-by: maomao.ling@intel.com <maomao.ling@intel.com>

--- ldap-client.js
+++ ldap-client.js
@@ -1,4 +1,3 @@
-//require "ldapjs"
 var ldap = require('ldapjs');
 
 ldap_port = 389
@@ -7,7 +6,7 @@ ldap_admin_password = 'cce@fhm@drsimon_2022_2'
 
 function login(username, password) {
   if (!username || !password) {
-  	console.log("Both idsid and Password are required!")
+    console.log("Both idsid and Password are required!")
   }
 
   console.log("username:", username)
@@ -16,71 +15,66 @@ function login(username, password) {
 
   ldap_host = 'ldap://ccr.corp.intel.com'
   console.log(ldap_host)
-  ldap_base_search = 'OU=Workers,DC=ccr,DC=corp,DC=intel,DC=com'
-  console.log("ldap_base_search:", ldap_base_search)
-  
-    //create client
-    var client = ldap.createClient({
-      url: ldap_host,
-      port: ldap_port
-    })
-    const dn = user_split[1]+'@ccr.corp.intel.com'
-  	console.log("dn:", dn)
-    var SearchSuccess = false
-    //bind search account
-    client.bind(dn, password, function (err, res1) {
-    //client.bind(user_split[1], ldap_admin_password, function (err, res1) {
-      if (err) {
-      console.log('Error is:', err)
-        console.log("connect error, bind error");
-		  /*
-      return reject({
-        error: "WRONG_CREDENTIAL",
-        message: `Incorrect username or password! (idsid, ccr\\maomaoli)`,
-      })
-	  */
-    }
-    })
 
-	user='maomaoli'
-	//user='aaahmed'
-	
-	/*
+  //create client
+  var client = ldap.createClient({
+    url: ldap_host,
+    port: ldap_port
+  })
+  const dn = user_split[1] + '@ccr.corp.intel.com'
+  console.log("dn:", dn)
+
+  ldap_base_search='OU=Workers,DC=amr,DC=corp,DC=intel,DC=com'
+  console.log("ldap_base_search:", ldap_base_search)
+
+  var SearchSuccess = false
+  //bind search account
+  client.bind(dn, password, function (err, res1) {
+    //client.bind(user_split[1], ldap_admin_password, function (err, res1) {
+    if (err) {
+      console.log("\nconnect error, bind error");
+      console.log('Error is:', err)
+    }
+  })
+
+  //user = 'maomaoli'
+  user='aaahmed'
+
+  /*
     var opts = {
       filter:'(&(memberOf=CN=FMTool-AD,OU=Managed,OU=Groups,DC=amr,DC=corp,DC=intel,DC=com)(sAMAccountName='+user+'))',
       scope: 'sub',     
       timeLimit: 500   
     }
-	*/
-    var opts = {
-      filter:'(sAMAccountName='+user+')',
-      scope: 'sub',     
-      timeLimit: 500   
-    }
+  */
+  var opts = {
+    filter: '(sAMAccountName='+user+')',
+    scope: 'sub',
+    timeLimit: 500
+  }
 
-    //client.search('ou=Workers,dc=amr,dc=corp,dc=intel,dc=com', opts, function (err, res2) {
-    client.search(ldap_base_search, opts, function (err, res2) {
-      res2.on('searchEntry', function (entry) {
-        SearchSuccess = true
-        var user = entry.object
-		console.log("user.dn:", user.dn)
-      })
-
-      //search error
-      res2.on('error', function (err) {
-        SearchSuccess = false
-        client.unbind()
-        console.log("You are not a member of FMTool-access, please register in AGS!")
-      })
-
-      //search end
-      res2.on('end', function (result) {
-        client.unbind()
-          console.log("end")
-      })
+  client.search(ldap_base_search, opts, function (err, res2) {
+    res2.on('searchEntry', function (entry) {
+      SearchSuccess = true
+      var user = entry.object
+      console.log("user.dn:", user.dn)
     })
+
+    //search error
+    res2.on('error', function (err) {
+      SearchSuccess = false
+      client.unbind()
+      console.log("not in LDAP")
+    })
+
+    //search end
+    res2.on('end', function (result) {
+      client.unbind()
+      console.log("end")
+    })
+  })
 }
 
-username=ldap_admin_user
-password=ldap_admin_password
+username = ldap_admin_user
+password = ldap_admin_password
 login(username, password)
